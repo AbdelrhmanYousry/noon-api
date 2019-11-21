bcd20ce389c746ec058bf9e188c7a67921b1899f
AbdelrhmanYousry <eng_abdelrhman93@yahoo.com>
2019-11-21 14:37:27 +0200

    Fixed err

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const userCtrl = require("../controllers/user.controller");
 const shootrCtrl = require("../controllers/photographer.controller");
 const categoryCtrl = require("../controllers/category.controller");
 const packageCtrl = require("../controllers/package.controller");
+const locationCtrl = require("../controllers/location.controller");
 const passport = require("passport")
 router.post("/signup", userCtrl.signUp)
 router.post("/login", userCtrl.logIn)
@@ -16,6 +17,8 @@ router.get("/events", passport.authenticate("jwt-user", { session: false}), user
 // admin apis
 router.post("/create-category", categoryCtrl.createCategory);
 router.post("/create-package", packageCtrl.createPackage);
+router.post("/create-location", locationCtrl.createLocation);
 router.get("/categories", categoryCtrl.getCategories);
 router.get("/packages", packageCtrl.getPackages);
+router.get("/locations", locationCtrl.getLocations);
 module.exports = router;
